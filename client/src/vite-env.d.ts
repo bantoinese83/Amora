a532e06fc7c56adb355d0bd202e25fd4690c1025
bantoinese83 <b.antoine.se@gmail.com>
2025-12-01 14:35:06 -0500

    Add VITE_GEMINI_API_KEY and VITE_API_URL to type definitions

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -5,6 +5,8 @@ interface ImportMetaEnv {
   readonly VITE_STRIPE_PRICE_ID_MONTHLY?: string;
   readonly VITE_STRIPE_PRICE_ID_YEARLY?: string;
   readonly VITE_BACKEND_URL?: string;
+  readonly VITE_API_URL?: string;
+  readonly VITE_GEMINI_API_KEY?: string;
   readonly VITE_NEON_DATABASE_URL?: string;
   readonly DEV?: boolean;
 }
